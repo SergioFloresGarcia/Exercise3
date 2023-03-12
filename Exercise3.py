59825f4e84619ecc2988258da569ffcfc4eabf9d
SergioFloresGarcia <1430727@senati.pe>
2023-03-12 15:35:10 -0500

    Exercise3

---
+++ Exercise3.py
@@ -0,0 +1,6 @@
+#escriba un codigo de programacion donde me permita imprimir el primer y el ultimo valor de la lista:
+list = ["red", "blue", "blak", "green", "white"]
+#primer elememnto: red
+#ultimo elemento: white
+print(list[0])
+print(list[-1])
